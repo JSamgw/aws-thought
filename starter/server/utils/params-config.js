25a98aab343ffb75872ae73f27e091b639be0288
Jessica Sam <grantjessie01@gmail.com>
2023-07-17 22:57:25 -0400

    Addtional change to thoughts

--- starter/server/utils/params-config.js
+++ starter/server/utils/params-config.js
@@ -1,5 +1,3 @@
-//This file will return the cofigured param object
-//uuid will generate a unique 36-character alphanumeric string
 const { v4: uuidv4 } = require('uuid');
 
 const params = (fileName) => {
@@ -7,12 +5,14 @@ const params = (fileName) => {
     const fileType = myFile[myFile.length - 1];
   
     const imageParams = {
-      Bucket: config.bucket,
+      // Replace the <My_Bucket_Name> with the name of your own S3 bucket
+      Bucket: 'user-images-9b76f6b4-4002-4887-9172-ba20edf015d5', 
       Key: `${uuidv4()}.${fileType}`,
       Body: fileName.buffer,
-      ACL: 'public-read', // allow read access to this file
+      ACL: 'public-read', // allows read access to this file, To access the images in the bucket from a public URL
     };
   
     return imageParams;
   };
+
   module.exports = params;
